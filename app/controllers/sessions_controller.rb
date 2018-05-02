21a231d8db79dc2ba2191661c104130dc0fb33e6
Sharmina Akhtar <sakht318@gmail.com>
2018-05-02 19:22:18 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
 
 
   def create
@@ -12,10 +14,5 @@ class SessionsController < ApplicationController
     end
   end
 
-
-  def destroy
-    session.delete :name if session[:name] = params[:name]
-    redirect_to '/logout'
-  end
 
 end
